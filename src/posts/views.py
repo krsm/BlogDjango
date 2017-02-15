d25e690960f401f8126df95d0b91fa0d01ff1bf2
krsm <krsm184@gmail.com>
2017-02-15 18:42:44 -0500

    added templates folder

--- src/posts/views.py
+++ src/posts/views.py
@@ -10,7 +10,8 @@ def post_detail(request): # retrieve
     return HttpResponse("<h1>Retrieve</h1>")
 
 def post_list(request):
-    return HttpResponse("<h1>List</h1>")
+    # return HttpResponse("<h1>List</h1>")
+    return render(request, "index.html", {})
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
