4571d04e2c3908b3a5f4c47bf2379a941f16d0f6
krsm <krsm184@gmail.com>
2017-02-14 22:46:45 -0500

    added code related to models

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    # auto_now_add True will updated when the variable is save initialy, just one time
+    # auto_now will be saved any time is saved
+
+    # If it was python 2 should be __unicode__
+    def __str__(self):
+        return self.title
