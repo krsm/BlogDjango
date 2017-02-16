e164e8aaa48596e817e9fd2d9fb5caa89924e430
krsm <krsm184@gmail.com>
2017-02-15 22:49:32 -0500

    added queryset basics

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 
 def post_create(request):
@@ -13,15 +15,21 @@ def post_detail(request): # retrieve
     return render(request, "index.html", context)
 
 def post_list(request):
+
+    queryset = Post.objects.all()
     # return HttpResponse("<h1>List</h1>")
-    if request.user.is_authenticated():
-        context = {
-            "title": "My User List"
-        }
-    else:
-        context = {
-            "title": "List"
-        }
+    context = {
+        "object_list": queryset,
+        "title": "My User List"
+    }
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My User List"
+    #     }
+    # else:
+    #     context = {
+    #         "title": "List"
+    #     }
     return render(request, "index.html", context)
 
 def post_update(request):
